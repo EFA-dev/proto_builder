64a4898f880d4106cf05f9bfd57bb5af00ea3abb
Haşim Yerlikaya <yerlikayahasim@gmail.com>
2020-08-15 22:03:24 +0300

    Verbose argument option added

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:args/args.dart';
 import 'package:glob/glob.dart';
 import 'package:logging/logging.dart';
 import 'package:proto_builder/src/protoc.dart';
@@ -8,15 +9,13 @@ import 'package:build_runner/src/logging/std_io_logging.dart';
 final protoFile = Glob('**.proto');
 
 void main(List<String> arguments) async {
-  Logger.root.level = Level.ALL;
-  Logger.root.onRecord.listen((record) {
-    stdout.write(colorLog(record, verbose: true));
-  });
+  final stopwatch = Stopwatch()..start();
 
+  checkArgs(arguments);
   var successful = <BuildResult>[];
   var failed = <BuildResult>[];
   var protoc = Protoc();
-  final stopwatch = Stopwatch()..start();
+
   await for (var entity in protoFile.list()) {
     var buildResult = await protoc.buildFile(entity.path);
     if (buildResult.result) {
@@ -35,7 +34,19 @@ void main(List<String> arguments) async {
   }
 
   Logger.root.info('************* ${stopwatch.elapsed.dayHourMinuteSecondFormatted()} Operation Time');
-  stopwatch.stop();
+}
+
+void checkArgs(List<String> arguments) {
+  var parser = ArgParser();
+  parser.addFlag('verbose', defaultsTo: true, abbr: 'v');
+
+  bool verbose = parser.parse(arguments)['verbose'];
+  if (verbose) {
+    Logger.root.level = Level.ALL;
+    Logger.root.onRecord.listen((record) {
+      stdout.write(colorLog(record, verbose: verbose));
+    });
+  }
 }
 
 extension DurationFormatter on Duration {
